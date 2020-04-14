dc93db219889e675baa36bd42e8e9d36892874ee
Eduardo Prasniewski <eduardoprasniewski@gmail.com>
2020-04-14 00:18:05 -0300

    Login controlller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,7 +4,7 @@ const keyController = require('./controllers/keyController');
 const familyController = require('./controllers/familyController');
 const queryController = require('./controllers/queryController');
 const pushController = require('./controllers/pushController');
-
+const loginController = require('./controllers/loginController');
 const routes = express.Router();
 
 routes.post('/key', keyController.create);
@@ -13,5 +13,6 @@ routes.get('/query', queryController.getShortcuts);
 routes.post('/query', queryController.search);
 routes.get('/query/search', queryController.path);
 routes.post('/user/push', pushController.create);
+routes.post('/login', loginController.create);
 
 module.exports = routes;
