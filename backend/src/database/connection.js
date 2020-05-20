82498e655fba737354b887881ba72cb57aaa6053
Eduardo Prasniewski <eduardoprasniewski@gmail.com>
2020-05-20 17:38:41 -0300

    GRAPHENDEDB

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,6 +1,5 @@
 const neo4j = require('neo4j-driver');
 require('dotenv').config();
 
-const driver =  neo4j.driver(process.env.APP_URI, neo4j.auth.basic(process.env.USER, process.env.PASSWORD));
-
+const driver =  neo4j.driver(process.env.APP_URI_GRAPHENEDB, neo4j.auth.basic(process.env.USER_GRAPHENEDB, process.env.PASSWORD_GRAPHENEDB),{ encrypted : true});
 module.exports = driver;
